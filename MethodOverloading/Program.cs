ee70ba7355cffbee0e09e59cf76418f5c4abe102
Kenton Knelle <kken51915@gmail.com>
2025-08-11 09:32:14 -0400

    Exercise 1 Initial Push

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -4,7 +4,49 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Add(2, 2);
+            Add(2.4m, 2.5m);
+            Add(0, 1, true);
         }
+        public static int Add(int numOne, int numTwo)
+        {
+            var numberSum = numOne + numTwo;
+            Console.WriteLine($"{numberSum}");
+            return numberSum;
+        }
+
+        public static decimal Add(decimal numOne, decimal numTwo)
+        {
+            var decimalSum = numOne + numTwo;
+            Console.WriteLine($"{decimalSum}");
+            return decimalSum;
+        }
+
+        public static String Add(int numOne, int numTwo, bool numIsTrue)
+        {
+            var sum = numOne + numTwo;
+            if (numIsTrue == true && sum > 1)
+            {
+                Console.WriteLine($"{sum} dollars");
+                return $"{sum} dollars";
+            }
+            else if (numIsTrue == true && sum == 1)
+            {
+                Console.WriteLine($"{sum} dollar");
+                return $"{sum} dollar";
+            }
+            else if (numIsTrue == true)
+                {
+                    Console.WriteLine($"{sum} dollars");
+                    return $"{sum} dollars";
+
+                }else
+            {
+                return sum.ToString();
+            }
+
+        }
+
     }
 }
+
